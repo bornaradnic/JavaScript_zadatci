9ce0d87b828e12d86e5e704987a73e56ccae8c92
bornaradnic <93705072+bornaradnic@users.noreply.github.com>
2021-12-16 09:27:57 +0100

    Add files via upload

---
+++ Java 2/main.js
@@ -0,0 +1,14 @@
+// Funkcija sa paremetrima
+
+//Program koji pozdravlja korisnika
+function pozdrav(ime) {
+
+    alert("Dobar dan " + ime + " :) ");
+
+}
+
+// Pitaj korisnika za ime
+let ime = prompt("Unesi svoje ime: ");
+
+// POzivanje funkcije
+pozdrav(ime);
---
+++ java/main.js
@@ -0,0 +1,34 @@
+// JavaScript funkcije
+
+// definiramo funciju
+
+function mojaFunkcija() {
+
+    alert("Ovo je poruka iz naše funkcije!");
+
+}
+// pozivamo funkciju
+// mojaFunkcija();
+
+function zbroji(a,b) {
+
+    c = a + b;
+    alert("Zbroj a i b je jednako: " + c);
+}
+
+// zbroji(4,3);
+// zbroji(53,69);
+
+let x = 0;
+
+function doseg(){
+
+    let x = 100;
+
+    alert(x);
+
+}
+
+alert(x);
+
+doseg();
